2117bb7fe1b3b50f22ba100503054c3bc3ff4104
nobody <moisesalex132@gmail.com>
2022-01-21 08:12:54 -0700

    test from github actions for jira

--- scripts/jira.sh
+++ scripts/jira.sh
@@ -1,28 +1,20 @@
 set -uo pipefail
-
-# if [[ -f "../output/scan/report.junit" ]]; then
-#     echo "Uploading report.junit file to XRay"
-#     project_key="TST13"
-#     token=$(curl -H "Content-Type: application/json" -X POST --data '{"client_id":"CD075895727E4E5DAFD5FCC1FE7A78BF","client_secret":"65344aaa49d1a15ff9f0d1c2353e1b80f6bba5b410c18f263e655ad8daf5ac23"}' https://xray.cloud.getxray.app/api/v2/authenticate | tr -d '"')
-#     curl -H "Content-Type: text/xml" -X POST -H "Authorization: Bearer $token" --data @"../output/scan/report.junit" https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey="TST13"
-# else
-#     echo "File report.junit not found"
-# fi
-
+if [[ -f "../output/scan/report.junit" ]]; then
+    echo "Uploading report.junit file to XRay"
+    project_key="TST34"
+    token=$(curl -H "Content-Type: application/json" -X POST --data '{"client_id":"CD075895727E4E5DAFD5FCC1FE7A78BF","client_secret":"65344aaa49d1a15ff9f0d1c2353e1b80f6bba5b410c18f263e655ad8daf5ac23"}' https://xray.cloud.getxray.app/api/v2/authenticate | tr -d '"')
+    curl -H "Content-Type: text/xml" -X POST -H "Authorization: Bearer $token" --data @"../output/scan/report.junit" https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey=${project_key} >response.txt
+else
+    echo "File report.junit not found"
+fi
+JIRA_TOKEN=$(echo -n moises@all-win.software:0LpFDh4o8FfeYppN8c4l79F7 | base64)
+execution_key=$(jq '.key' response.txt | tr -d '"')
 NOW=$(date +"%D")
 summary="Test Execution result for ${GITHUB_ACTOR} en_us: ${NOW}  "
 description="executed in: ${GITHUB_RUN_ID} More details are available at: /output/scan/"
-<<<<<<< HEAD
-execution_data="$(jq --arg s "$summary" --arg d "$description" '.update .summary[] .set = $s | .fields.description = $d' ./issue_template.json)"
+execution_data="$(jq --arg s "$summary" --arg d "$description" '.update .summary[] .set = $s | .fields.description = $d' ../scripts/issue_template.json)"
 
-TOKEN_BASIC=$(echo -n moises@all-win.software:2lYTcJkXntdIGAuMlc7y187F | base64)
-=======
-execution_data="$(jq --arg s "$summary" --arg d "$description" '.update .summary[] .set = $s | .fields.description = $d' ./scripts/issue_template.json)"
-
-TOKEN_BASIC=$(echo -n moises@all-win.software:BtRR4NY8jJFcuaBUCHvI9256 | base64)
->>>>>>> 351e2a47f7d5ffc5b494ea11f7e9b2fe77b3d359
-
-curl -H "Authorization: Basic ${TOKEN_BASIC}" \
+curl -H "Authorization: Basic ${JIRA_TOKEN}" \
     -X PUT --data "${execution_data}" \
     -H "Content-Type: application/json" \
-    https://moisesa.atlassian.net/rest/api/2/issue/TST13-201
+     https://moisesa.atlassian.net/rest/api/2/issue/$execution_keys
