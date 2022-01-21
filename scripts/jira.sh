106ef8b8e4cc7850da3b8e9c61e64ee26313f194
Moisés Masis <moisesalex132@gmail.com>
2022-01-21 08:19:02 -0700

    Update jira.sh

--- scripts/jira.sh
+++ scripts/jira.sh
@@ -1,5 +1,5 @@
 set -uo pipefail
-if [[ -f "../output/scan/report.junit" ]]; then
+if [[ -f "./output/scan/report.junit" ]]; then
     echo "Uploading report.junit file to XRay"
     project_key="TST34"
     token=$(curl -H "Content-Type: application/json" -X POST --data '{"client_id":"CD075895727E4E5DAFD5FCC1FE7A78BF","client_secret":"65344aaa49d1a15ff9f0d1c2353e1b80f6bba5b410c18f263e655ad8daf5ac23"}' https://xray.cloud.getxray.app/api/v2/authenticate | tr -d '"')
@@ -12,7 +12,7 @@ execution_key=$(jq '.key' response.txt | tr -d '"')
 NOW=$(date +"%D")
 summary="Test Execution result for ${GITHUB_ACTOR} en_us: ${NOW}  "
 description="executed in: ${GITHUB_RUN_ID} More details are available at: /output/scan/"
-execution_data="$(jq --arg s "$summary" --arg d "$description" '.update .summary[] .set = $s | .fields.description = $d' ../scripts/issue_template.json)"
+execution_data="$(jq --arg s "$summary" --arg d "$description" '.update .summary[] .set = $s | .fields.description = $d' ./scripts/issue_template.json)"
 
 curl -H "Authorization: Basic ${JIRA_TOKEN}" \
     -X PUT --data "${execution_data}" \
