9c62118091424a5805e13ea6f6f0866f3c8f4d02
Moisés Masis <moisesalex132@gmail.com>
2022-01-24 07:45:47 -0700

    Update jira.sh

--- scripts/jira.sh
+++ scripts/jira.sh
@@ -9,7 +9,7 @@ else
     echo "File report.junit not found"
 fi
 
-JIRA_TOKEN=$(echo -n moises@all-win.software:nh19vJKs9gKUNP2FdKLw6C9B| base64)
+JIRA_TOKEN=$(echo -n moises@all-win.software:$JIRA_TOKEN_ENV| base64)
 execution_key=$(jq '.key' response.txt | tr -d '"')
 NOW=$(date +"%D")
 summary="Test Execution result for en_us: ${NOW}  "
