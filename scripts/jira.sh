94a4dbf471773b7e83d56c41b6e24b0667048668
Moisés Masis <moisesalex132@gmail.com>
2022-01-26 10:46:36 -0700

    Update jira.sh

--- scripts/jira.sh
+++ scripts/jira.sh
@@ -2,7 +2,7 @@
 set -uo pipefail
 if [[ -f "./output/scan/report.junit" ]]; then
     echo "Uploading report.junit file to XRay"
-    project_key="TST34"
+    project_key="TST1"
     token=$(curl -H "Content-Type: application/json" -X POST --data '{"client_id":"CD075895727E4E5DAFD5FCC1FE7A78BF","client_secret":"65344aaa49d1a15ff9f0d1c2353e1b80f6bba5b410c18f263e655ad8daf5ac23"}' https://xray.cloud.getxray.app/api/v2/authenticate | tr -d '"')
     curl -H "Content-Type: text/xml" -X POST -H "Authorization: Bearer $token" --data @"./output/scan/report.junit" https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey=${project_key} >response.txt
 else
